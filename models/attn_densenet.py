b174b0cf0893570aef0e105b3a1068df1b2bd7a9
Abhishek K Das <abhishek.das.contact@gmail.com>
2019-05-16 15:07:26 -0500

    add: densenet + attention model

---
+++ models/attn_densenet.py
@@ -0,0 +1,122 @@
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+from densenet import DenseBlock, _Transition
+from attention import MultiHeadAttention
+
+
+class AttnDenseNet(nn.Module):
+    r"""Densenet-BC + Attention model class, based on
+
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+    """
+
+    def __init__(
+            self,
+            growth_rate=32,
+            block_config=(
+                6,
+                12,
+                24,
+                16),
+            num_init_features=64,
+            bn_size=4,
+            drop_rate=0,
+            num_classes=1000,
+            num_heads=8,
+            total_key_filters=32,
+            total_value_filters=32,
+            output_filters=32):
+
+        super(AttnDenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                num_heads=num_heads,
+                total_key_filters=total_key_filters,
+                total_value_filters=total_value_filters,
+                output_filters=output_filters)
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate + output_filters
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=num_features // 2)
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = num_features // 2
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+
+def total_params(model):
+    return sum(p.numel() for p in model.parameters())
+
+
+# sample random input
+if __name__ == '__main__':
+    x = torch.rand(2, 3, 224, 224)
+    num_classes = 3
+
+    net = AttnDenseNet(growth_rate=32,
+                       block_config=(
+                           6,
+                           12,
+                           24,
+                           16),
+                       num_init_features=64,
+                       bn_size=4,
+                       drop_rate=0,
+                       num_classes=3,
+                       num_heads=4,
+                       total_key_filters=16,
+                       total_value_filters=16,
+                       output_filters=8)
+    print(total_params(net))
+    y = net(x)
+    print(y.shape)
---
+++ models/densenet.py
@@ -0,0 +1,102 @@
+#
+# This file contains the densenet building blocks
+# Components have taken from the pytorch implementation
+# of densenet and minor modifications wherever necessary.
+# Link to pytorch implementation:
+#   https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py
+#
+
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+from attention import MultiHeadAttention
+
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module(
+            'conv1',
+            nn.Conv2d(
+                num_input_features,
+                bn_size *
+                growth_rate,
+                kernel_size=1,
+                stride=1,
+                bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                                           kernel_size=3, stride=1, padding=1,
+                                           bias=False)),
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        new_features = super(_DenseLayer, self).forward(x)
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate,
+                                     training=self.training)
+        return torch.cat([x, new_features], 1)
+
+
+class DenseBlock(nn.Module):
+    def __init__(
+            self,
+            num_layers,
+            num_input_features,
+            bn_size,
+            growth_rate,
+            drop_rate,
+            num_heads,
+            total_key_filters,
+            total_value_filters,
+            output_filters):
+        super(DenseBlock, self).__init__()
+        self.denselayers = nn.ModuleDict()
+        self.multi_attn = MultiHeadAttention(
+            num_heads=num_heads,
+            num_input_channels=num_input_features,
+            total_key_filters=total_key_filters,
+            total_value_filters=total_value_filters,
+            output_filters=output_filters)
+
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features +
+                i *
+                growth_rate,
+                growth_rate,
+                bn_size,
+                drop_rate)
+            self.denselayers.add_module('denselayer%d' % (i + 1), layer)
+
+    def forward(self, x):
+        # attend to the global features
+        attn = self.multi_attn(x)
+
+        for layer in self.denselayers:
+            x = self.denselayers[layer](x)
+
+        # concatentate the attn features with denselayer output
+        return torch.cat([x, attn], 1)
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module(
+            'conv',
+            nn.Conv2d(
+                num_input_features,
+                num_output_features,
+                kernel_size=1,
+                stride=1,
+                bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
